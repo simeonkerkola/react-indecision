5e6095762078f6419614f109a71ad6ec27d80481
sssmi <simeon.kerkola@gmail.com>
2017-10-14 21:51:17 +0300

    Refactor some components

--- src/components/Options.jsx
+++ src/components/Options.jsx
@@ -2,24 +2,21 @@ import React from 'react'
 import Option from './Option.jsx'
 
 // Render new tag to each option
-const Options = (props) => {
-  const options = props.options
-  return (
-    <div>
-      {/* <p>You have: {options.length} options</p> */}
-      <button onClick={props.handleDeleteOptions}>Remove All</button>
-      <br/>
-      {options.length ? <label>Your options: </label> : <label>No options yet</label>}
-      {
-        options.map(option => (
-          <Option
-            key={option} optionText={option}
-            handleDeleteOption={props.handleDeleteOption}
-          />
-        ))
-      }
-    </div>
-  )
-}
+const Options = (props) => (
+  <div>
+    {/* <p>You have: {options.length} options</p> */}
+    <button onClick={props.handleDeleteOptions}>Remove All</button>
+    <br/>
+    {props.options.length ? <label>Your options: </label> : <label>No options yet</label>}
+    {
+      props.options.map(option => (
+        <Option
+          key={option} optionText={option}
+          handleDeleteOption={props.handleDeleteOption}
+        />
+      ))
+    }
+  </div>
+)
 
 export default Options
